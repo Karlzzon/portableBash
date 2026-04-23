edbb6cc3f611a4cfdb449ba6bbb5a6acc0cfa071
Oskar Karlsson <90701772+Karlzzon@users.noreply.github.com>
2026-04-23 11:46:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,4 +8,3 @@ cp "$DOTFILES_DIR/portableBash.sh" "$HOME/portableBash.sh"
 if ! grep -qF 'portableBash.sh' "$HOME/.bashrc" 2>/dev/null; then
     echo '[ -f ~/portableBash.sh ] && source ~/portableBash.sh' >> "$HOME/.bashrc"
 fi
-source "$HOME/.bashrc"
